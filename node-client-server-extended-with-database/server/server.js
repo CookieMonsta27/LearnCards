58a9bf69f0c6eaee73b7db3b935f35e9aba032bb
krassertuep <schwenkschuster.finn@gmail.com>
2022-04-11 15:40:23 +0200

    Funktioniert alles jetzt, aber nur so semi

--- node-client-server-extended-with-database/server/server.js
+++ node-client-server-extended-with-database/server/server.js
@@ -146,6 +146,23 @@ app.get('/database2', (req, res) => {
     });
 });
 
+app.get('/database3', (req, res) => {
+    console.log("Request to load all entries from rightstack");
+    // Prepare the get query
+    connection3.query("SELECT * FROM `card_right`;", function (error, results, fields) {
+        if (error) {
+            // we got an errror - inform the client
+            console.error(error); // <- log error in server
+            res.status(500).json(error); // <- send to client
+        } else {
+            // we got no error - send it to the client
+            console.log('Success answer from DB3: ', results); // <- log results in console
+            // INFO: Here could be some code to modify the result
+            res.status(200).json(results); // <- send it to client
+        }
+    });
+});
+
 // DELETE path for database
 app.delete('/database/:id', (req, res) => {
     // This path will delete an entry. For example the path would look like DELETE '/database/5' -> This will delete number 5
@@ -155,6 +172,50 @@ app.delete('/database/:id', (req, res) => {
     // Actual executing the query to delete it from the server
     // Please keep in mind to secure this for SQL injection!
     connection1.query("DELETE FROM `cardstack` WHERE `cardstack`.`task_id` = " + id + ";", function (error, results, fields) {
+        if (error) {
+            // we got an errror - inform the client
+            console.error(error); // <- log error in server
+            res.status(500).json(error); // <- send to client
+        } else {
+            // Everything is fine with the query
+            console.log('Success answer: ', results); // <- log results in console
+            // INFO: Here can be some checks of modification of the result
+            res.status(200).json(results); // <- send it to client
+        }
+    });
+});
+
+// DELETE path for databasefalse if question guess right
+app.delete('/databasefalse/:id', (req, res) => {
+    // This path will delete an entry. For example the path would look like DELETE '/database/5' -> This will delete number 5
+    let id = req.params.id; // <- load the ID from the path
+    console.log("Request to delete Item: " + id); // <- log for debugging
+
+    // Actual executing the query to delete it from the server
+    // Please keep in mind to secure this for SQL injection!
+    connection2.query("DELETE FROM `card_false` WHERE `card_false`.`task_id` = " + id + ";", function (error, results, fields) {
+        if (error) {
+            // we got an errror - inform the client
+            console.error(error); // <- log error in server
+            res.status(500).json(error); // <- send to client
+        } else {
+            // Everything is fine with the query
+            console.log('Success answer: ', results); // <- log results in console
+            // INFO: Here can be some checks of modification of the result
+            res.status(200).json(results); // <- send it to client
+        }
+    });
+});
+
+// DELETE path for databaseright if question guess wrong
+app.delete('/databaseright/:id', (req, res) => {
+    // This path will delete an entry. For example the path would look like DELETE '/database/5' -> This will delete number 5
+    let id = req.params.id; // <- load the ID from the path
+    console.log("Request to delete Item: " + id); // <- log for debugging
+
+    // Actual executing the query to delete it from the server
+    // Please keep in mind to secure this for SQL injection!
+    connection3.query("DELETE FROM `card_right` WHERE `card_right`.`task_id` = " + id + ";", function (error, results, fields) {
         if (error) {
             // we got an errror - inform the client
             console.error(error); // <- log error in server
@@ -215,6 +276,40 @@ app.post('/addfalse', (req, res) => {
         // Actual executing the query. Please keep in mind that this is for learning and education.
         // In real production environment, this has to be secure for SQL injection!
         connection2.query("INSERT INTO `card_false` (`task_id`, `title`, `description`, `created_at`) VALUES (" + task_id + ", '" + title + "', '" + description + "', current_timestamp());", function (error, results, fields) {
+            if (error) {
+                // we got an errror - inform the client
+                console.error(error); // <- log error in server
+                res.status(500).json(error); // <- send to client
+            } else {
+                // Everything is fine with the query
+                console.log('Success answer: ', results); // <- log results in console
+                // INFO: Here can be some checks of modification of the result
+                res.status(200).json(results); // <- send it to client
+            }
+        });
+    }
+    else {
+        // There is nobody with a title nor description
+        console.error("Client send no correct data!")
+        // Set HTTP Status -> 400 is client error -> and send message
+        res.status(400).json({ message: 'This function requries a body with "title" and "description' });
+    }
+});
+
+// Add Right ones into db2
+app.post('/addright', (req, res) => {
+    // This will add a new row. So we're getting a JSON from the webbrowser which needs to be checked for correctness and later
+    // it will be added to the database with a query.
+    if (typeof req.body !== "undefined" && typeof req.body.task_id !== "undefined" && typeof req.body.title !== "undefined" && typeof req.body.description !== "undefined") {
+        // The content looks good, so move on
+        // Get the content to local variables:
+        var task_id = req.body.task_id;
+        var title = req.body.title;
+        var description = req.body.description;
+        console.log("Client send database insert request with id:" + task_id + "title: " + title + " ; description: " + description); // <- log to server
+        // Actual executing the query. Please keep in mind that this is for learning and education.
+        // In real production environment, this has to be secure for SQL injection!
+        connection3.query("INSERT INTO `card_right` (`task_id`, `title`, `description`, `created_at`) VALUES (" + task_id + ", '" + title + "', '" + description + "', current_timestamp());", function (error, results, fields) {
             if (error) {
                 // we got an errror - inform the client
                 console.error(error); // <- log error in server
