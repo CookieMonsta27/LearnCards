1fbcb935eb7d0f5f88d451d659f0eef403d8beea
krassertuep <schwenkschuster.finn@gmail.com>
2022-04-23 15:15:15 +0200

    unnötige Sachen rausgehauen

--- node-client-server-extended-with-database/server/server.js
+++ node-client-server-extended-with-database/server/server.js
@@ -73,39 +73,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Entrypoint - call it with: http://localhost:8080/ -> redirect you to http://localhost:8080/static
-app.get('/', (req, res) => {
-    console.log("Got a request and redirect it to the static page");
-    // redirect will send the client to another path / route. In this case to the static route.
-    res.redirect('/static');
-});
 
-// Another GET Path - call it with: http://localhost:8080/special_path
-app.get('/special_path', (req, res) => {
-    res.send('This is another path');
-});
-
-// Another GET Path that shows the actual Request (req) Headers - call it with: http://localhost:8080/request_info
-app.get('/request_info', (req, res) => {
-    console.log("Request content:", req)
-    res.send('This is all I got from the request:' + JSON.stringify(req.headers));
-});
-
-// POST Path - call it with: POST http://localhost:8080/client_post
-app.post('/client_post', (req, res) => {
-    if (typeof req.body !== "undefined" && typeof req.body.post_content !== "undefined") {
-        var post_content = req.body.post_content;
-        console.log("Client send 'post_content' with content:", post_content)
-        // Set HTTP Status -> 200 is okay -> and send message
-        res.status(200).json({ message: 'I got your message: ' + post_content });
-    }
-    else {
-        // There is no body and post_contend
-        console.error("Client send no 'post_content'")
-        // Set HTTP Status -> 400 is client error -> and send message
-        res.status(400).json({ message: 'This function requries a body with "post_content"' });
-    }
-});
 
 
 
